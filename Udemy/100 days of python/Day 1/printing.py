fd24697c3056ea30326e3f280ef4d2236c007268
valeriu coman <0comav27@solent.ac.uk>
2025-10-26 13:22:58 +0000

    add variables to print

--- Udemy/100 days of python/Day 1/printing.py
+++ Udemy/100 days of python/Day 1/printing.py
@@ -3,3 +3,21 @@ print("""1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
 3. Add 3g of Salt.
 4. Leave to rise for 2 hours.
 5. Bake at 200 degrees C for 30 minutes.""")
+
+
+# Greeting the user
+
+#print("hello" + " " + input(" What is your name? ") + "!")
+
+
+
+# Counting the lenght of the user name
+
+#user=input("What is your name? ")
+#print("your user lenght is"+" " +str(len(user))) 
+
+#Final version
+
+username =input("What is your name? ")
+lenght = len(username)
+print("Hello " + username + "! Your name has " + str(lenght) + " characters.")
