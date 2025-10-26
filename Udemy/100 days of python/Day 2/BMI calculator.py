2e0b4738cf3c4af77cbdf7775510113c9ffdb40b
valeriu coman <0comav27@solent.ac.uk>
2025-10-26 14:11:21 +0000

    do a BMI calc

---
+++ Udemy/100 days of python/Day 2/BMI calculator.py
@@ -0,0 +1,17 @@
+#The body mass index (BMI) is a measure used in medicine
+# to see if someone is underweight or overweight. This is the
+# formula used to calculate it:
+#bmi is equal to the person's weight divided by 
+#the person's height squared.
+#Convert this sentence into code on line 6.
+
+
+
+
+height = 1.65 
+weight = 84
+
+
+bmi =weight / height ** 2    
+
+print(bmi)
