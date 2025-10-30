bc348e46fddd5613ab9bdc5cdbb39e2d0c7d10c9
Valeriu Coman <0comav27@solent.ac.uk>
2025-10-30 09:51:15 +0000

    Update ticket_machine.py
    add age check and prices

--- Udemy/100 days of python/Day 3/ticket_machine.py
+++ Udemy/100 days of python/Day 3/ticket_machine.py
@@ -4,6 +4,13 @@ height = float(input("Please enter your height in cm:   "))
 
 if height < 120:
     print("Sorry, you have to be at least 120 cm to ride.")
+    age = int(input("Please enter your age:  "))
+    if age < 12:
+        print("Your ticket price is $5.")
+    elif age <= 18:
+        print("Your ticket price is $7.")
+    else:
+        print("Your ticket price is $12.")
 elif height >= 120 and height < 160:
     print("You can ride the rollercoaster.")
 else:
