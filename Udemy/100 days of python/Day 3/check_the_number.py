5d8532601e544cc73e56cb1016d610c7b4a6f4a2
Valeriu Coman <0comav27@solent.ac.uk>
2025-10-30 09:43:35 +0000

    Create check_the_number.py

---
+++ Udemy/100 days of python/Day 3/check_the_number.py
@@ -0,0 +1,8 @@
+#check if the number is even or odd
+
+
+number = int(input("Please enter a number:  "))
+if number % 2 == 0:
+    print(f"Your number {number} is even.")
+else:
+    print(f"Your number {number} is odd.")
