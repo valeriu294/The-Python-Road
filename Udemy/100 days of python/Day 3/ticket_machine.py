f61a9fde32ef05342bd00e6560fa8aafac80c930
Valeriu Coman <0comav27@solent.ac.uk>
2025-10-30 09:29:39 +0000

    Create ticket_machine.py

---
+++ Udemy/100 days of python/Day 3/ticket_machine.py
@@ -0,0 +1,13 @@
+print("Welcome to the Ticket Machine!")
+height = float(input("Please enter your height in cm:   "))
+
+
+if height < 120:
+    print("Sorry, you have to be at least 120 cm to ride.")
+elif height >= 120 and height < 160:
+    print("You can ride the rollercoaster.")
+else:
+    print("You can ride but you need to pay an extra fee of $5.")
+break
+
+    
